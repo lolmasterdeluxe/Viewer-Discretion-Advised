43015005010304251d231d0c65423a1277d90738
HikamaoSG <94425049+HikamaoSG@users.noreply.github.com>
2026-01-31 04:00:24 +0800

    Updated and added player scripts plus fixes
    Added player scripts.
    Core mechanic should be working now at least 90% working
    NPC, Collisions and Players script are supposed to work with each other

--- Assets/Scripts/Collision/EnumCollisionHandler2D.cs
+++ Assets/Scripts/Collision/EnumCollisionHandler2D.cs
@@ -1,43 +1,91 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class EnumCollisionHandler2D : MonoBehaviour
 {
+    private CollidableType myType;
+
+    [Header("Possession – collision detection only")]
+    public bool canPossess { get; private set; } = false;
+    public GameObject CurrentPossessableNPC { get; private set; } = null;
+    public NPCData CurrentPossessableNPCData { get; private set; } = null;
+    private void Awake()
+    {
+        myType = GetComponent<CollidableType>();
+        if(myType == null)
+        {
+            Debug.LogError("CollidableType component not found on " + gameObject.name);
+            enabled = false;
+            
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D collision) // Physical collisions
     {
-        CollidableObject2D collidable = collision.collider.GetComponent<CollidableObject2D>();
-        if(collidable != null)
-        {
-            switch (collidable.types)
-            {
-                case CollidableTypes.PlayerMask:
-                    Debug.Log("Collided with PlayerMask");
-                    //Add custom behaviour here
-                    break;
 
-                case CollidableTypes.NPC:
-                    Debug.Log("Collided with NPC");
-                    //Add custom behaviour here
-                    break;
-               
-                case CollidableTypes.Obstacle:
-                    Debug.Log("Collided with Obstacle");
-                    //Add custom behaviour here
-                    break;
-                
-                case CollidableTypes.Projectile:
-                    Debug.Log("Collided with Projectile");
-                    //Add custom behaviour here
-                    break;
-                
-                case CollidableTypes.Melee:
-                    Debug.Log("Collided with Melee Attack");
-                    //Add custom behaviour here 
+        var otherType = collision.collider.GetComponent<CollidableType>();
+        if (otherType == null) return;
+
+        // myType is based on object this script is attached to
+        // otherType is based on the object we collided with
+
+        switch (myType.types)
+        {
+            case CollidableTypes.PlayerMask:
+                switch (otherType.types)
+                {
+                    case CollidableTypes.Projectile:
+                        Debug.Log("Player hit by bullet! Take damage / knockback");
+                        // TakeDamage();
+                        break;
+
+                    case CollidableTypes.NPC:
+                        NPCData npcData = GetNPCData(collision.gameObject);
+                        if (npcData != null)
+                        {
+                            CurrentPossessableNPCData = npcData;
+                            CurrentPossessableNPC = collision.gameObject;
+                            canPossess = true;
+                            Debug.Log("Player bumped into NPC → maybe dialogue / push?");
+                        }
+                        break;
+
+                    case CollidableTypes.Obstacle:
+                        Debug.Log("Player ran into wall");
+                        break;
+
+                    case CollidableTypes.Melee:
+                        Debug.Log("Player hit by melee attack!");
+                        break;
+                }
                 break;
-                default:
-                    Debug.Log("Collided with Unknown Type");
-                    break;
-            }
         }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        var otherTypeComp = collision.collider.GetComponent<CollidableType>();
+        if (otherTypeComp == null) return;
+
+        if (otherTypeComp.types == CollidableTypes.NPC &&
+            collision.gameObject == CurrentPossessableNPC)
+        {
+            canPossess = false;
+            CurrentPossessableNPC = null;
+            CurrentPossessableNPCData = null;
+            Debug.Log("[Possession] Left NPC range");
+        }
+    }
+
+    private NPCData GetNPCData(GameObject go)
+    {
+        var holder = go.GetComponent<NPCDataHolder>();
+        if (holder != null)
+        {
+            return holder.Data;
+        }
+
+        // Optional fallbacks if you ever add it differently later
+        return null;
     }
 
 }
